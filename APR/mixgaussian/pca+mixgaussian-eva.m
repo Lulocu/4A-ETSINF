1f2f2e7c9f4ab509f8a14c74a1a248fa79a55125
sheldonicus <lulocu99@gmail.com>
2021-01-13 22:20:49 +0100

    Preparando memoria

--- APR/mixgaussian/pca+mixgaussian-eva.m
+++ APR/mixgaussian/pca+mixgaussian-eva.m
@@ -28,4 +28,8 @@ proyX = (X -m) * trDataPCA(:,1:pcaKs);
 proyY = (Y-m)*trDataPCA(:,1:pcaKs);
 
 edv = mixgaussian(proyX,xl,proyY,yl,K,alpha);
-printf("%.1e %3d %3d %6.3f\n",alpha,pcaKs,K,edv);
+printf("%.1e %3d %3d %6.3f\n\n\n",alpha,pcaKs,K,edv);
+
+acurancia = (1 -edv /100);
+intervalo=1.96*sqrt((edv /100*acurancia)/length(yl));
+printf(" Error:%.4d \t Intervalo de confianza [%.4d , %4d]\n",edv,edv - intervalo, edv + intervalo);
